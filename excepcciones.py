6280d1503c9d8df04291cd56351ae1cc7787fe23
kimberly2009-3 <garcia.kimberly.2024@cbtis-246.edu.mx>
2025-02-28 20:22:14 -0600

    Add files via upload

---
+++ excepcciones.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+x =10 / 0
+
+
+# In[9]:
+
+
+print(x)
+
+
+# In[11]:
+
+
+lista =[1,2,3]
+print(lista[4])
+
+
+# In[1]:
+
+
+print("Hola, mundo"
+
+
+# In[5]:
+
+
+"hola" + 5
+
+
+# In[7]:
+
+
+int("abc")
+
+
+# In[9]:
+
+
+diccionario = {"a": 1, "b": 2}
+print(diccionario["c"])
+
+
+# In[11]:
+
+
+open("archivo_inexistente.txt", "r")
+
+
+# In[25]:
+
+
+import math
+math.exp(1000)
+
+
+# In[7]:
+
+
+texto = "¡Hola, mundo! 😊"
+with open("archivo.txt", "w", encoding="ascii") as f:
+    f.write(texto)
+
