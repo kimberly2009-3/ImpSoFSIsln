49d7dac75d2dd19152b441d02e8ae3558cae9828
kimberly2009-3 <garcia.kimberly.2024@cbtis-246.edu.mx>
2025-04-08 19:12:02 -0600

    Add files via upload

---
+++ sqlite/Activivad5.py
@@ -0,0 +1,15 @@
+import sqlite3
+from DataBaseManager import DataBaseManager
+
+dataBaseManager = DataBaseManager()
+
+
+con = sqlite3.connect(dataBaseManager.database)
+materia=(4,'matematicas','kimberly',9)
+dataBaseManager.create_materias(con, materia)
+cur = con.cursor()
+cur.execute("select * from materias;")
+table_list =cur.fetchall()
+for row in table_list:
+    print(row)
+    con.close()
